c57e513444d4761652fc9c7f153237c63b4bd12b
slawomir <onewebdesignpl@gmail.com>
2021-06-25 16:38:27 +0200

    Single product page and api

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,12 +1,70 @@
 <template>
   <div class="home">
-    Home page
+    <section class="hero is-medium is-dark mb-6">
+      <div class="hero-body has-text-centered">
+        <p class="title mb-6">
+          Welcome to OneShop
+        </p>
+        <p class="subtitle">
+          The best online store template
+        </p>
+      </div>
+    </section>
+
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">Latest products</h2>
+      </div>
+
+      <div
+        class="column is-3"
+        v-for="product in latestProducts"
+        :key="product.id"
+      >
+        <div class="box">
+          <figure class="image mb-4">
+            <img :src="product.get_thumbnail" alt="feature-product" />
+          </figure>
+
+          <h3 class="is-size-4">{{ product.name }}</h3>
+          <p class="is-size-6 has-text-grey">${{ product.price }}</p>
+
+          <router-link
+            :to="product.get_absolute_url"
+            class="button is-dark mt-4"
+            >View details</router-link
+          >
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Home",
   components: {},
+  data() {
+    return {
+      latestProducts: [],
+    };
+  },
+  mounted() {
+    this.getLatestProducts();
+  },
+  methods: {
+    getLatestProducts() {
+      axios
+        .get("/api/v1/latest-products")
+        .then((response) => {
+          this.latestProducts = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
--- ecommerce_template/product/serializers.py
+++ ecommerce_template/product/serializers.py
@@ -7,6 +7,7 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = (
             "id",
+            "category",
             "name",
             "get_absolute_url",
             "description",
