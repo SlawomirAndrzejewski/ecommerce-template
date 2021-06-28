3c6fdef7fab0fbfc96880544c9b4d10337f8be20
slawomir <slawomir.and1@gmail.com>
2021-06-28 12:46:58 +0200

    no message

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,48 @@
 <template>
   <div id="wrapper">
-    <Navbar />
+    <nav class="navbar is-dark">
+    <div class="navbar-brand">
+      <router-link to="/" class="navbar-item">
+        <strong>OneShop</strong>
+      </router-link>
+
+      <a
+        @click="showMobileMenu = !showMobileMenu"
+        class="navbar-burger"
+        aria-label="menu"
+        aria-expanded="false"
+        data-target="navbar-menu"
+      >
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div
+      id="navbar-menu"
+      class="navbar-menu"
+      :class="{ 'is-active': showMobileMenu }"
+    >
+      <div class="navbar-end">
+        <router-link to="/summer" class="navbar-item">Summer</router-link>
+        <router-link to="/winter" class="navbar-item">Winter</router-link>
+
+        <div class="navbar-item">
+          <div class="buttons">
+            <router-link to="/log-in" class="button is-light"
+              >Log in</router-link
+            >
+
+            <router-link to="/cart" class="button is-success">
+              <span class="icon"><i class="fas fa-shopping-cart"></i></span>
+              <span>Cart ({{ cartTotalLength }})</span>
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
 
     <section class="section">
       <router-view />
@@ -22,6 +64,7 @@ export default {
   },
   data() {
     return {
+       showMobileMenu: false,
       cart: {
         items: [],
       },
